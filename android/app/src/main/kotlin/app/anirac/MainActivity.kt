afa7f0ec589ff7166c96cf4781118c274fac7a84
neiljaywarner <njwandroid@gmail.com>
2022-01-03 23:01:47 -0600

    Initial commit with very_good_cli and firebase cli with flavors

---
+++ android/app/src/main/kotlin/app/anirac/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.anirac.ourverses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2021, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ourverses/app/app.dart';
+import 'package:ourverses/bootstrap.dart';
+import 'package:ourverses/firebase_options_dev.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await bootstrap(() => const App());
+}
