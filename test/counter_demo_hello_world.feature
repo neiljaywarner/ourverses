a3e56a2c9d0769c7ae3a5749d2327e166324a7ad
neiljaywarner <njwandroid@gmail.com>
2022-01-21 16:32:06 -0600

    OUR-44 Both tests in group pass, can also tap on button

--- test/counter_demo_hello_world.feature
+++ test/counter_demo_hello_world.feature
@@ -2,3 +2,8 @@ Feature: Counter
     Scenario: Initial counter value is 0
         Given the app is running
         Then I see {'0'} text
+
+    Scenario: Plus button increases the counter
+        Given the app is running
+        When I tap {Icons.add} icon
+        Then I see {'1'} text
--- test/counter_demo_hello_world_test.dart
+++ test/counter_demo_hello_world_test.dart
@@ -6,12 +6,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 import './step/the_app_is_running.dart';
 import './step/i_see_text.dart';
+import './step/i_tap_icon.dart';
 
 void main() {
   group('''Counter''', () {
     testWidgets('''Initial counter value is 0''', (tester) async {
       await theAppIsRunning(tester);
       await iSeeText(tester, '0');
+    });
+    testWidgets('''Plus button increases the counter''', (tester) async {
+      await theAppIsRunning(tester);
+      await iTapIcon(tester, Icons.add);
+      await iSeeText(tester, '1');
     });
   });
 }
