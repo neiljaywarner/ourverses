a88d779a0611896f8c2fbc7608acffc803d2a29d
neiljaywarner <njwandroid@gmail.com>
2022-01-21 15:34:44 -0600

    Fixes OUR-46

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -12,9 +12,10 @@ class HomeScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final _selectedIndex = useState(0);
+    final appBarTitle =  (_selectedIndex.value == 0) ? 'Home' : 'Verses';
 
     return Scaffold(
-    appBar: AppBar(title: const Text('Our Verses'),),
+    appBar: AppBar(title: Text(appBarTitle),),
     bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home',),
